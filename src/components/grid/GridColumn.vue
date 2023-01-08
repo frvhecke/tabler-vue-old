64d12c04c1978176458e7eff53f5b49937bfd1c0
frvhecke <frederik@cluttr.be>
2023-01-08 14:02:13 +0100

    Update to grid components

--- src/components/grid/GridColumn.vue
+++ src/components/grid/GridColumn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="[ class_col_sm, class_col_md, class_col_lg, class_col_xl, class_col_xxl, class_col_sm_auto, class_col_md_auto, class_col_lg_auto, class_col_xl_auto, class_col_xxl_auto, class_col_x, class_col_sm_x, class_col_md_x, class_col_lg_x, class_col_xl_x, class_col_xxl_x, class_mb ]">
+  <div :class="[ class_col_sm, class_col_md, class_col_lg, class_col_xl, class_col_xxl, class_col_sm_auto, class_col_md_auto, class_col_lg_auto, class_col_xl_auto, class_col_xxl_auto, class_col_x, class_col_sm_x, class_col_md_x, class_col_lg_x, class_col_xl_x, class_col_xxl_x, class_mb, class_py, getBackgroundColor() ]">
     <slot></slot>
   </div>
 </template>
@@ -24,7 +24,9 @@ export default {
     col_lg_x: { default: 0, type: Number },
     col_xl_x: { default: 0, type: Number },
     col_xxl_x: { default: 0, type: Number },
-    mb: { default: 0, type: Number }
+    mb: { default: 0, type: Number },
+    py: { default: undefined, type: Number },
+    bg_color: { default: undefined, type: String }
   },
   computed: {
     class_col_sm () {
@@ -77,6 +79,86 @@ export default {
     },
     class_mb () {
       return this.mb > 0 ? 'mb-' + this.mb : ''
+    },
+    class_py () {
+      if (this.py === undefined) return ''
+      else if (this.py >= 0) return 'py-' + this.py
+      else if (this.py < 0) return 'py-n' + Math.abs(this.py)
+      else return ''
+    }
+  },
+  methods: {
+    getBackgroundColor () {
+      if (this.bg_color === 'primary') {
+        return 'bg-primary'
+      } else if (this.bg_color === 'secondary') {
+        return 'bg-secondary'
+      } else if (this.bg_color === 'success') {
+        return 'bg-success'
+      } else if (this.bg_color === 'warning') {
+        return 'bg-warning'
+      } else if (this.bg_color === 'danger') {
+        return 'bg-danger'
+      } else if (this.bg_color === 'info') {
+        return 'bg-info'
+      } else if (this.bg_color === 'light') {
+        return 'bg-light'
+      } else if (this.bg_color === 'dark') {
+        return 'bg-dark'
+      } else if (this.bg_color === 'blue') {
+        return 'bg-blue'
+      } else if (this.bg_color === 'azure') {
+        return 'bg-azure'
+      } else if (this.bg_color === 'indigo') {
+        return 'bg-indigo'
+      } else if (this.bg_color === 'purple') {
+        return 'bg-purple'
+      } else if (this.bg_color === 'pink') {
+        return 'bg-pink'
+      } else if (this.bg_color === 'red') {
+        return 'bg-red'
+      } else if (this.bg_color === 'orange') {
+        return 'bg-orange'
+      } else if (this.bg_color === 'yellow') {
+        return 'bg-yellow'
+      } else if (this.bg_color === 'lime') {
+        return 'bg-lime'
+      } else if (this.bg_color === 'green') {
+        return 'bg-green'
+      } else if (this.bg_color === 'teal') {
+        return 'bg-teal'
+      } else if (this.bg_color === 'cyan') {
+        return 'bg-cyan'
+      } else if (this.bg_color === 'facebook') {
+        return 'bg-facebook'
+      } else if (this.bg_color === 'twitter') {
+        return 'bg-twitter'
+      } else if (this.bg_color === 'google') {
+        return 'bg-google'
+      } else if (this.bg_color === 'youtube') {
+        return 'bg-youtube'
+      } else if (this.bg_color === 'vimeo') {
+        return 'bg-vimeo'
+      } else if (this.bg_color === 'dribbble') {
+        return 'bg-dribbble'
+      } else if (this.bg_color === 'github') {
+        return 'bg-github'
+      } else if (this.bg_color === 'instagram') {
+        return 'bg-instagram'
+      } else if (this.bg_color === 'pinterest') {
+        return 'bg-pinterest'
+      } else if (this.bg_color === 'vk') {
+        return 'bg-vk'
+      } else if (this.bg_color === 'rss') {
+        return 'bg-rss'
+      } else if (this.bg_color === 'flickr') {
+        return 'bg-flickr'
+      } else if (this.bg_color === 'bitbucket') {
+        return 'bg-bitbucket'
+      } else if (this.bg_color === 'tabler') {
+        return 'bg-tabler'
+      }
+      return ''
     }
   }
 }
--- src/components/grid/GridRow.vue
+++ src/components/grid/GridRow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :class="[ { 'row-cards' : row_cards } ]">
+  <div class="row" :class="[ { 'row-cards' : row_cards }, margin_top, margin_bottom, gutter, align_items_class ]">
     <slot></slot>
   </div>
 </template>
@@ -8,7 +8,39 @@
 export default {
   name: 'GridRow',
   props: {
-    row_cards: { default: false, type: Boolean }
+    row_cards: { default: false, type: Boolean },
+    mt: { default: undefined, type: Number },
+    mb: { default: undefined, type: Number },
+    g: { default: undefined, type: Number },
+    align_items: { default: undefined, type: String } // 'start', 'center', 'end', 'stretch'
+  },
+  computed: {
+    margin_top () {
+      if (this.mt === 0) return 'mt-0'
+      else if (this.mt > 0) return 'mt-' + this.mt
+      else if (this.mt < 0) return 'mt-n' + Math.abs(this.mt)
+      else return ''
+    },
+    margin_bottom () {
+      if (this.mb === 0) return 'mb-0'
+      else if (this.mb > 0) return 'mb-' + this.mb
+      else if (this.mb < 0) return 'mb-n' + Math.abs(this.mb)
+      else return ''
+    },
+    gutter () {
+      if (this.g === 0) return 'g-0'
+      else if (this.g > 0) return 'g-' + this.g
+      else if (this.g < 0) return 'g-n' + Math.abs(this.g)
+      else return ''
+    },
+    align_items_class () {
+      if (this.align_items === undefined) return ''
+      else if(this.align_items === 'start') return 'align-items-start'
+      else if(this.align_items === 'center') return 'align-items-center'
+      else if(this.align_items === 'end') return 'align-items-end'
+      else if(this.align_items === 'stretch') return 'align-items-stretch'
+      else return ''
+    }
   }
 }
 </script>
