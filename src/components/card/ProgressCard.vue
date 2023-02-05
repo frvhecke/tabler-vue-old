012f473c98c2b1f6db67a32e784e47c68f4c7611
frvhecke <frederik@cluttr.be>
2023-02-05 14:25:53 +0100

    Break out ProgressCard

---
+++ src/components/card/ProgressCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <CardWrapper :progressbar="true" :progress="progress">
+    <template #header v-if="header_active">
+      <slot name="header"></slot>
+    </template>
+    <slot></slot>
+    <template #footer v-if="footer_active">
+      <slot name="footer"></slot>
+    </template>
+  </CardWrapper>
+</template>
+
+<script>
+import CardWrapper from '@/components/card/CardWrapper.vue'
+
+export default {
+  name: 'StackedCard',
+  components: {
+    CardWrapper
+  },
+  props: {
+    progress: { default: 0, type: Number }
+  },
+  computed: {
+    footer_active () {
+      return this.$slots.footer
+    },
+    header_active () {
+      return this.$slots.header
+    }
+  }
+}
+</script>
