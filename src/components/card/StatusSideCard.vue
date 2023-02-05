908fee7eb95853eda23810748f1e7c3b1cce1640
frvhecke <frederik@cluttr.be>
2023-02-05 14:21:19 +0100

    Break out Status cards

---
+++ src/components/card/StatusSideCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <CardWrapper status="side" :status_color="color">
+    <template #header v-if="header_active">
+      <slot name="header"></slot>
+    </template>
+    <slot></slot>
+    <template #footer v-if="footer_active">
+      <slot name="footer"></slot>
+    </template>
+  </CardWrapper>
+</template>
+
+<script>
+import CardWrapper from '@/components/card/CardWrapper.vue'
+
+export default {
+  name: 'StatusSideCard',
+  components: {
+    CardWrapper
+  },
+  props: {
+    color: { default: '', type: String }
+  },
+  computed: {
+    footer_active () {
+      return this.$slots.footer
+    },
+    header_active () {
+      return this.$slots.header
+    }
+  }
+}
+</script>
