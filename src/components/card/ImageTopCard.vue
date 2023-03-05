3c4f2e57fe01789e768be935f2bc9cbde4b50efd
frvhecke <frederik@cluttr.be>
2023-03-05 07:07:11 +0100

    Adding ImageTopCard

---
+++ src/components/card/ImageTopCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <CardWrapper :background_color="background_color" :text_color="text_color" :text_color_auto="text_color_auto">
+    <div class="img-responsive img-responsive-21x9 card-img-top" :style="image_style"></div>
+    <slot></slot>
+  </CardWrapper>
+</template>
+
+<script>
+import CardWrapper from '@/components/card/CardWrapper.vue'
+
+export default {
+  name: 'ImageTopCard',
+  components: {
+    CardWrapper
+  },
+  props: {
+    background_color: { default: '', type: String },
+    text_color: { default: '', type: String },
+    text_color_auto: { default: false, type: Boolean },
+    image_url: { default: '', type: String },
+    image_alt: { default: '', type: String }
+  },
+  computed: {
+    image_style () {
+      return 'background-image: url(' + this.image_url + ')'
+    }
+  }
+}
+</script>
