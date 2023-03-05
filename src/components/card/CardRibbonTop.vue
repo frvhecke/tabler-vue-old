755bc04c41540de2ed684ba63574a1d89a6ae247
frvhecke <frederik@cluttr.be>
2023-03-05 06:33:39 +0100

    Color cleanup

--- src/components/card/CardRibbonTop.vue
+++ src/components/card/CardRibbonTop.vue
@@ -14,83 +14,16 @@ export default {
     bg_color: { default: 'yellow', type: String },
     icon: { default: 'star', type: String }
   },
+  data: () => ({
+    colors: new Array('primary','secondary','success','warning','danger','info','light','dark','blue','azure','indigo','purple','pink','red','orange','yellow','lime','green','teal','cyan','facebook','twitter','google','youtube','vimeo','dribbble','github','instagram','pinterest','vk','rss','flickr','bitbucket','tabler')
+  }),
   components: {
     StandardIcon
   },
   methods: {
     getBackgroundColor () {
-      if (this.bg_color === 'primary') {
-        return 'bg-primary'
-      } else if (this.bg_color === 'secondary') {
-        return 'bg-secondary'
-      } else if (this.bg_color === 'success') {
-        return 'bg-success'
-      } else if (this.bg_color === 'warning') {
-        return 'bg-warning'
-      } else if (this.bg_color === 'danger') {
-        return 'bg-danger'
-      } else if (this.bg_color === 'info') {
-        return 'bg-info'
-      } else if (this.bg_color === 'light') {
-        return 'bg-light'
-      } else if (this.bg_color === 'dark') {
-        return 'bg-dark'
-      } else if (this.bg_color === 'blue') {
-        return 'bg-blue'
-      } else if (this.bg_color === 'azure') {
-        return 'bg-azure'
-      } else if (this.bg_color === 'indigo') {
-        return 'bg-indigo'
-      } else if (this.bg_color === 'purple') {
-        return 'bg-purple'
-      } else if (this.bg_color === 'pink') {
-        return 'bg-pink'
-      } else if (this.bg_color === 'red') {
-        return 'bg-red'
-      } else if (this.bg_color === 'orange') {
-        return 'bg-orange'
-      } else if (this.bg_color === 'yellow') {
-        return 'bg-yellow'
-      } else if (this.bg_color === 'lime') {
-        return 'bg-lime'
-      } else if (this.bg_color === 'green') {
-        return 'bg-green'
-      } else if (this.bg_color === 'teal') {
-        return 'bg-teal'
-      } else if (this.bg_color === 'cyan') {
-        return 'bg-cyan'
-      } else if (this.bg_color === 'white') {
-        return 'bg-white'
-      } else if (this.bg_color === 'facebook') {
-        return 'bg-facebook'
-      } else if (this.bg_color === 'twitter') {
-        return 'bg-twitter'
-      } else if (this.bg_color === 'google') {
-        return 'bg-google'
-      } else if (this.bg_color === 'youtube') {
-        return 'bg-youtube'
-      } else if (this.bg_color === 'vimeo') {
-        return 'bg-vimeo'
-      } else if (this.bg_color === 'dribbble') {
-        return 'bg-dribbble'
-      } else if (this.bg_color === 'github') {
-        return 'bg-github'
-      } else if (this.bg_color === 'instagram') {
-        return 'bg-instagram'
-      } else if (this.bg_color === 'pinterest') {
-        return 'bg-pinterest'
-      } else if (this.bg_color === 'vk') {
-        return 'bg-vk'
-      } else if (this.bg_color === 'rss') {
-        return 'bg-rss'
-      } else if (this.bg_color === 'flickr') {
-        return 'bg-flickr'
-      } else if (this.bg_color === 'bitbucket') {
-        return 'bg-bitbucket'
-      } else if (this.bg_color === 'tabler') {
-        return 'bg-tabler'
-      }
-      return ''
+      // Return matching bg-color or empty if no match
+      return this.colors.includes(this.bg_color) ? 'bg-' + this.bg_color : ''
     }
   }
 }
