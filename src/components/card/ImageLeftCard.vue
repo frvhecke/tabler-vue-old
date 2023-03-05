d4c3433f42222ed81a81bb3edcd7df22b117ec0c
frvhecke <frederik@cluttr.be>
2023-03-05 07:02:16 +0100

    Adding ImageLeftCard

---
+++ src/components/card/ImageLeftCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <CardWrapper :background_color="background_color" :text_color="text_color" :text_color_auto="text_color_auto">
+    <div class="row row-0">
+      <div class="col-3" v-if="image_url">
+        <img :src="image_url" class="w-100 h-100 object-cover card-img-start" :alt="image_alt">
+      </div>
+      <div class="col">
+        <slot></slot>
+      </div>
+    </div>
+  </CardWrapper>
+</template>
+
+<script>
+import CardWrapper from '@/components/card/CardWrapper.vue'
+
+export default {
+  name: 'ImageLeftCard',
+  components: {
+    CardWrapper
+  },
+  props: {
+    background_color: { default: '', type: String },
+    text_color: { default: '', type: String },
+    text_color_auto: { default: false, type: Boolean },
+    image_url: { default: '', type: String },
+    image_alt: { default: '', type: String }
+  }
+}
+</script>
