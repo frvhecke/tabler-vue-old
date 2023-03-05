1112564b9764c939504cdc6e70ca36285ffbb044
frvhecke <frederik@cluttr.be>
2023-03-05 12:25:25 +0100

    Adding card placeholder + card body padding toggle

--- src/components/card/CardBody.vue
+++ src/components/card/CardBody.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="card-body">
+  <div class="card-body" :class="[ { 'p-0' : !padding } ]">
     <div><slot></slot></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CardBody'
+  name: 'CardBody',
+  props: {
+    padding: { default: true, type: Boolean }
+  }
 }
 </script>
---
+++ src/components/card/CardPlaceholder.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" class="w-100" preserveAspectRatio="none" :width="width" :height="height" :viewBox="viewbox" fill="transparent" :stroke="stroke">
+    <line x1="0" y1="0" :x2="width" :y2="height"></line>
+    <line x1="0" :y1="height" :x2="width" y2="0"></line>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'CardPlaceholder',
+  props: {
+    width: { default: 100, type: Number },
+    height: { default: 100, type: Number },
+    color: { default: 'b8cef1', type: String }
+  },
+  computed: {
+    viewbox () {
+      return "0 0 " + this.width + " " + this.height
+    },
+    stroke () {
+      return 'var(--tblr-border-color, #' + this.color + ')'
+    }
+  }
+}
+</script>
