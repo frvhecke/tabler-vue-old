9e649a42dc40ee0d4c6db93008e01739489cc5ca
frvhecke <frederik@cluttr.be>
2023-03-05 11:16:09 +0100

    Adding left/right slots

--- src/components/card/CardFooter.vue
+++ src/components/card/CardFooter.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="card-footer" :class="[ { 'card-footer-transparent' : transparent } ]">
+    <div class="d-flex" v-if="left_active || right_active">
+      <div v-if="left_active">
+        <slot name="left"></slot>
+      </div>
+      <div class="ms-auto" v-if="right_active">
+        <slot name="right"></slot>
+      </div>
+    </div>
+    <div v-if="default_active">
       <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,6 +19,17 @@ export default {
   name: 'CardFooter',
   props: {
     transparent: { default: false, type: Boolean }
+  },
+  computed: {
+    left_active () {
+      return this.$slots.left
+    },
+    right_active () {
+      return this.$slots.right
+    },
+    default_active () {
+      return this.$slots.default
+    }
   }
 }
 </script>
