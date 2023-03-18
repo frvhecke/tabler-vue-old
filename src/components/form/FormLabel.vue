c9ca84174d714fd3894c0c2ff9c8c7cb5639add9
frvhecke <frederik@cluttr.be>
2023-03-18 10:04:36 +0100

    Adding form label

---
+++ src/components/form/FormLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <label class="form-label" :class="[ { 'required' : required } ]">
+    {{text}}
+    <span v-if="description" class="form-label-description">{{description}}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'FormLabel',
+  props: {
+    text: { default: '', type: String },
+    description: { default: '', type: String },
+    required: { default: false, type: Boolean }
+  }
+}
+</script>
