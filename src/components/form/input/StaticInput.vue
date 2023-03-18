dee7d03dee5b91f841a28e869c108527f2a8d3eb
frvhecke <frederik@cluttr.be>
2023-03-18 10:43:42 +0100

    Adding static input

---
+++ src/components/form/input/StaticInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="form-control-plaintext">{{value}}</div>
+</template>
+
+<script>
+export default {
+  name: 'StaticInput',
+  props: {
+    value: { default: '', type: String }
+  }
+}
+</script>
