b90d91501d5172b83436b4a1cc17622c2610c93d
frvhecke <frederik@cluttr.be>
2023-03-18 10:04:45 +0100

    Adding form element

---
+++ src/components/form/FormElement.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-3">
+    <FormLabel :text="label" :required="required" />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import FormLabel from '@/components/form/FormLabel.vue'
+
+export default {
+  name: 'FormElement',
+  components: {
+    FormLabel
+  },
+  props: {
+    label: { default: '', type: String },
+    description: { default: '', type: String },
+    required: { default: false, type: Boolean }
+  }
+}
+</script>
