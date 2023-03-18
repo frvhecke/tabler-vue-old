31b80580d98d32e7439f25f189cb41490c3af333
frvhecke <frederik@cluttr.be>
2023-03-18 10:43:52 +0100

    Adding text input

---
+++ src/components/form/input/TextInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" :name="name" v-model="text" class="form-control" :placeholder="placeholder" :disabled="disabled" :readonly="readonly" @input="update()">
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    placeholder: { default: '', type: String },
+    name: { default: 'text', type: String },
+    value: { default: '', type: String },
+    disabled: { default: false, type: Boolean },
+    readonly: { default: false, type: Boolean }
+  },
+  data: () => ({
+    text: ''
+  }),
+  methods: {
+    update (e) {
+      this.$emit('input', this.text)
+    }
+  }
+}
+</script>
