f16afac218036d08109eca19076aecdeec8a10f0
frvhecke <frederik@cluttr.be>
2023-04-08 11:27:41 +0200

    Adding Select inputs

---
+++ src/components/form/input/SelectInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <select class="form-select" v-model="value" @change="change()">
+    <SelectInputOption v-for="option in options" :key="option.value" :text="option.name" :value="option.value" :selected="option.selected" />
+  </select>
+</template>
+
+<script>
+import SelectInputOption from '@/components/form/input/SelectInputOption.vue'
+
+export default {
+  name: 'SelectInput',
+  components: {
+    SelectInputOption
+  },
+  props: {
+    name: { default: 'text', type: String },
+    options: { default: null, type: Array }
+  },
+  data: () => ({
+    value: []
+  }),
+  methods: {
+    change () {
+      this.$emit('input', this.value)
+    }
+  }
+}
+</script>
---
+++ src/components/form/input/SelectInputOption.vue
@@ -0,0 +1,22 @@
+<template>
+  <option :value="value" :selected="selected">{{text}}</option>
+</template>
+
+<script>
+export default {
+  name: 'SelectInput',
+  props: {
+    text: { default: '', type: String },
+    value: { default: '', type: String },
+    selected: { default: false, type: Boolean }
+  },
+  data: () => ({
+    val: ''
+  }),
+  methods: {
+    change () {
+      this.$emit('input', this.value)
+    }
+  }
+}
+</script>
