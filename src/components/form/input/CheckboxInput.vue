0768f52faa0504ce18107437fb2306889ba3c36b
frvhecke <frederik@cluttr.be>
2023-04-08 11:27:27 +0200

    Adding checkbox & radio inputs

---
+++ src/components/form/input/CheckboxInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <label class="form-check" :class="[ { 'form-check-inline' : inline } ]">
+    <input class="form-check-input" type="checkbox" :name="name" :checked="checked" :disabled="disabled" @change="update()">
+    <span class="form-check-label">{{ label }}</span>
+    <span v-if="description" class="form-check-description">
+      {{ description }}
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxInput',
+  props: {
+    label: { default: '', type: String },
+    name: { default: 'text', type: String },
+    checked: { default: false, type: Boolean },
+    disabled: { default: false, type: Boolean },
+    inline: { default: false, type: Boolean },
+    description: { default: '', type: String }
+  },
+  methods: {
+    update (e) {
+      this.$emit('change', e)
+    }
+  }
+}
+</script>
---
+++ src/components/form/input/RadioInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <label class="form-check" :class="[ { 'form-check-inline' : inline } ]">
+    <input class="form-check-input" type="radio" :name="name" :checked="checked" :disabled="disabled" @change="update()">
+    <span class="form-check-label">{{ label }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'RadioInput',
+  props: {
+    label: { default: '', type: String },
+    name: { default: 'text', type: String },
+    checked: { default: false, type: Boolean },
+    disabled: { default: false, type: Boolean },
+    inline: { default: false, type: Boolean }
+  },
+  methods: {
+    update (e) {
+      this.$emit('change', e)
+    }
+  }
+}
+</script>
